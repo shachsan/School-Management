f34119ac37d76e78d0ff3d6a9952c1ff007e9846
Sanjay Sai <shachsan@hotmail.com>
2019-01-18 14:33:59 -0500

    start calendar

---
+++ school-management-frontend/src/components/Calendar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import dateFns from "date-fns";
+
+class Calendar extends React.Component {
+    state = {
+        currentMonth: new Date(),
+        selectedDate: new Date()
+    };
+
+    renderHeader() {
+        console.log(this.state.currentMonth);
+        const dateFormat = "MMMM YYYY";
+        return (
+          <div className="header row flex-middle">
+            <div className="col col-start">
+              <div className="icon" onClick={this.prevMonth}>
+                chevron_left
+              </div>
+            </div>
+            <div className="col col-center">
+              <span>
+                {dateFns.format(this.state.currentMonth, dateFormat)}
+              </span>
+            </div>
+            <div className="col col-end" onClick={this.nextMonth}>
+              <div className="icon">chevron_right</div>
+            </div>
+          </div>
+        );
+      }
+
+    
+
+    renderDays() {}
+
+    renderCells() {}
+
+    onDateClick = day => {};
+
+    nextMonth = () => {};
+
+    prevMonth = () => {};
+
+  render() {
+    return (
+        <div className="calendar">
+            {this.renderHeader()}
+            {this.renderDays()}
+            {this.renderCells()}
+        </div>
+    );
+  }
+}
+
+export default Calendar;
